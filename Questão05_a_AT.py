a9419ef8aa5dd1003a389ef2d66ee4f42f766579
marianabsctba <62853909+marianabsctba@users.noreply.github.com>
2020-09-21 20:10:47 -0300

    Update Questão05_a_AT.py

--- Questão05_a_AT.py
+++ Questão05_a_AT.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-
 #Questão 05 A realizada separadamente. Por favor, insira os arquivos da Questão 05 na mesma pasta do arquivo assessment_PIB.csv.
+
+import matplotlib.pyplot as plt
 
 
 def dados_arquivo():
@@ -13,11 +13,22 @@ def dados_arquivo():
     return lista_paises
 
 
+def lin():
+    print("\033[1;34m==\033[0;0m" * 40)
+
+
+def menu():
+    lin()
+    print("\033[1;33m\nOlá, seja bem-vindo(a) ao WorldPIB. "
+          "Você poderá checar o PIB de alguns países do mundo. Vamos lá?\n\033[0;0m")
+    lin()
+
+
 def digite_ano():
     ano_usuario = str(input("Digite um ano (entre 2013 e 2020): ")).strip()
     lista_paises = dados_arquivo()
     if ano_usuario not in lista_paises['país']:
-        print("Ano inválido. Programa reiniciado.")
+        print("\033[1;31mAno inválido. Programa reiniciado.\033[0;0m")
         exit(1)
     return ano_usuario
 
@@ -26,24 +37,20 @@ def digite_pais():
     pais_usuario = str(input("Digite um país: ")).lower().strip()
     lista_paises = dados_arquivo()
     if pais_usuario not in lista_paises:
-        print("País inválido.")
+        lin()
+        print("\033[1;31mPaís inválido. Programa reiniciado.\033[0;0m")
+        lin()
         exit(1)
     if pais_usuario == "país":
-        print("País inválido.Programa reiniciado.")
+        lin()
+        print("\033[1;31mPaís inválido.Programa reiniciado.\033[0;0m")
+        lin()
         exit(1)
     return pais_usuario
 
 
 def get_variacao(paises, pais_digitado):
     return ((float(paises[pais_digitado][-1]) / float(paises[pais_digitado][0])) - 1) * 100
-
-
-def grafico(x_axis, y_axis):
-    plt.plot(x_axis, y_axis)
-    plt.xlabel('Anos')
-    plt.ylabel('Valor em trilhões (US$)')
-    plt.title('PIB')
-    plt.show()
 
 
 def retorna_pib():
@@ -53,6 +60,10 @@ def retorna_pib():
 
     index = int(ano_digitado) - 2013
     pib_pais = paises[pais_digitado][index]
+    lin()
     print(f"O PIB do {pais_digitado.upper()} em {ano_digitado} é US$ {pib_pais} trilhões.")
+    lin()
 
+
+menu()
 retorna_pib()
