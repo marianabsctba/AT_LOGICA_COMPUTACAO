d89c3e57ad3f37ce2d10f3c940098c349f60dfed
marianabsctba <62853909+marianabsctba@users.noreply.github.com>
2020-09-21 11:32:32 -0300

    Create Questão05_b_AT.py

---
+++ Questão05_b_AT.py
@@ -0,0 +1,22 @@
+
+#Essa é a segunda versão da Questão 05, mas feita separadamente.
+
+def dados_arquivo():
+    arquivo = open('assessment_PIB.csv', 'r', encoding='utf-8')
+    arquivo = arquivo.read()
+    lista_paises = {}
+    for linha in arquivo.splitlines():
+        valores = linha.split(',')
+        lista_paises[valores[0].strip()] = valores[1:]
+    return lista_paises
+
+
+def get_variacao():
+    lista_paises = dados_arquivo()
+    lista_paises.pop('País')
+    for pais in lista_paises:
+        variacao = ((float(lista_paises[pais][-1]) / float(lista_paises[pais][0])) - 1) * 100
+        print(f"A variação do PIB do {pais} entre 2013 a 2020 é de US$ {variacao:.2f} trilhões.")
+
+
+get_variacao()
