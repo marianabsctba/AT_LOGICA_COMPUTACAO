b1a9e7127cc9327e4d91a3e15e36d27a23b6f990
marianabsctba <62853909+marianabsctba@users.noreply.github.com>
2020-09-21 20:26:41 -0300

    Update Questão05_c_AT.py

--- Questão05_c_AT.py
+++ Questão05_c_AT.py
@@ -1,4 +1,4 @@
-mport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 
 
@@ -12,23 +12,17 @@ def dados_arquivo():
     return lista_paises
 
 
-def digite_ano():
-    ano_usuario = str(input("Digite um ano (entre 2013 e 2020): ")).strip()
-    lista_paises = dados_arquivo()
-    if ano_usuario not in lista_paises['país']:
-        print("Ano inválido. Programa reiniciado.")
-        exit(1)
-    return ano_usuario
-
-
 def digite_pais():
-    pais_usuario = str(input("Digite um país: ")).lower().strip()
+    print("\033[1;34m==\033[0;0m" * 30)
+    pais_usuario = str(input("Digite um país para saber a evolução do PIB pelo tempo: ")).lower().strip()
+    print("\033[1;34m==\033[0;0m" * 30)
+
     lista_paises = dados_arquivo()
     if pais_usuario not in lista_paises:
-        print("País inválido.")
+        print("\033[1;31mPaís inválido. Programa reiniciado.\033[0;0m")
         exit(1)
     if pais_usuario == "país":
-        print("País inválido.Programa reiniciado.")
+        print("\033[1;31mPaís inválido. Programa reiniciado.\033[0;0m")
         exit(1)
     return pais_usuario
 
@@ -48,10 +42,13 @@ def retorna_grafico():
     y_axis = [float(valor) for valor in paises[pais_digitado]]
 
     time.sleep(2)
-    print(f"Agora veja o gráfico com a evolução do PIB do país {pais_digitado.upper()}, de 2013 a 2020.")
+    print(f"\033[1;34mAgora veja o gráfico com a evolução do PIB do país: {pais_digitado.upper()}, "
+          f"de 2013 a 2020.\033[0;0m")
     time.sleep(3)
     grafico(x_axis, y_axis)
 
 
-
 retorna_grafico()
+print("\033[1;34m==\033[0;0m" * 30)
+print("FIM. Volte sempre!")
+print("\033[1;34m==\033[0;0m" * 30)
